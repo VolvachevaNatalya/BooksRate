a87bbca9acf7dd292bc84d1c6e92817ed8abd263
VolvachevaNatalya <vnatalya2006@gmail.com>
2021-10-21 08:47:19 +0300

    Rate the song

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SongsComponent } from './songs/songs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongsComponent
   ],
   imports: [
     BrowserModule
